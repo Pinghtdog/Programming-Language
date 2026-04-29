d5a2d3868f9323479079820a5da1ce3794f188b2
keps <kierulfkelvin@gmail.com>
2026-04-29 12:25:40 +0800

    added file handling

--- Main.java
+++ Main.java
@@ -1,22 +1,35 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        String sourceCode = "let a = 10;\n" +
-                            "let b = 25;\n" +
-                            "print \"The answer is:\";\n" +
-                            "print a * b;\n";
-        
-        System.out.println("Source code:\n" + sourceCode);
-        System.out.println("-------------------------------------");
+        String filePath = "test.txt";
 
-        Lexer lexer = new Lexer(sourceCode);
-        List<Token> tokens = lexer.scanTokens();
+        if (args.length > 0) {
+            filePath = args[0];
+        }
 
-        Parser parser = new Parser(tokens);
-        List<Stmt> statements = parser.parse();
+        try {
+            byte[] bytes = Files.readAllBytes(Paths.get(filePath));
+            String sourceCode = new String(bytes);
 
-        Interpreter interpreter = new Interpreter();
-        interpreter.interpret(statements); 
+            System.out.println("Running file: " + filePath);
+            System.out.println("-------------------------------------");
+
+            Lexer lexer = new Lexer(sourceCode);
+            List<Token> tokens = lexer.scanTokens();
+
+            Parser parser = new Parser(tokens);
+            List<Stmt> statements = parser.parse();
+
+            Interpreter interpreter = new Interpreter();
+            interpreter.interpret(statements);
+
+        } catch (IOException e) {
+            System.err.println("Error: Could not read file '" + filePath + "'");
+            System.err.println("Make sure the file exists in the same folder.");
+        }
     }
 }
